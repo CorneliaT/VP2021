fb09d91ffe4ffb0f67f27151a03956e3a9ac2e3d
CorneliaT <44434374+CorneliaT@users.noreply.github.com>
2022-01-09 22:15:30 +0200

    Add files via upload

---
+++ Tund5/fnc_user.php
@@ -0,0 +1,49 @@
+<?php
+    $database = "if21_cornelia";
+    
+    function sign_up($firstname, $surname, $email, $gender, $birth_date, $password){
+        $notice = null;
+        $conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"]);
+        $conn->set_charset("utf8");
+        $stmt = $conn->prepare("INSERT INTO vpr_users (firstname, lastname, birthdate, gender, email, password) VALUES(?,?,?,?,?,?)");
+        echo $conn->error;
+        $option = ["cost"=>12];
+        $pwd_hash = password_hash($password, PASSWORD_BCRYPT, $option);
+        $stmt->bind_param("sssiss", $firstname, $surname, $birth_date, $gender, $email, $pwd_hash);
+        if($stmt->execute()){
+            $notice = "Uus kasutaja edukalt loodud!";
+        } else {
+            $notice = "Uue kasutaja loomisel tekkis viga: " .$stmt->error;
+        }
+        $stmt->close();
+        $conn->close();
+        return $notice;
+    }
+    
+    function sign_in($email, $password){
+        $notice = null;
+        $conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"]);
+        $conn->set_charset("utf8");
+        $stmt = $conn->prepare("SELECT id, firstname, lastname, password FROM vpr_users WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->bind_result($id_from_db, $firstname_from_db, $lastname_from_db, $password_from_db);
+        echo $conn->error;
+        $stmt->execute();
+        if($stmt->fetch()){
+            
+            if(password_verify($password, $password_from_db)){
+                $stmt->close();
+                $conn->close();
+                header("Location: home.php");
+                exit();
+            } else {
+                $notice = "Kasutajanimi või salasõna oli vale!";
+            }
+        } else {
+            $notice = "Kasutajanimi või salasõna oli vale!";
+        }
+        
+        $stmt->close();
+        $conn->close();
+        return $notice; 
+    }
